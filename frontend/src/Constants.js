a471e4793137cd0226f37378936c7f42c85f807c
Rajesh <budidirajeshgoud231@gmail.com>
2024-07-12 16:52:54 +0530

    Update Constants.js

--- frontend/src/Constants.js
+++ frontend/src/Constants.js
@@ -1,2 +1,2 @@
-export const BASE_URL = process.env.NODE_ENV === 'development' ? 'http://localhost:5000' : '';
+export const BASE_URL = process.env.NODE_ENV === 'development' ? 'http://localhost:5000' : 'https://thetitan-858v.onrender.com';
 export const USER_URL = '/api/userRouter';
