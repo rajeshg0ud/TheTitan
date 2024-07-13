2d592b53db73d707965136980ae79f2488c18bf7
Rajesh <budidirajeshgoud231@gmail.com>
2024-07-13 22:42:29 +0530

    Update generateToken.js

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -9,9 +9,9 @@ const generateToken = (res, userId) => {
     // Set token as an HTTP-only cookie
     res.cookie('jwt', token, {
         httpOnly: true,
-        secure: process.env.NODE_ENV === 'production', // true for production
-        sameSite: 'strict', // Required for cross-origin requests
-        maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days
+        secure: process.env.NODE_ENV !== 'development',  
+        sameSite: 'none',  
+        maxAge: 30 * 24 * 60 * 60 * 1000,  
     });
 
     // Set status and response message
