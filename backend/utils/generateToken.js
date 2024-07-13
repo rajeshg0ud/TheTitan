1d57dcbb2ebcba9f522a1ffefcf4160bd9a5a404
Rajesh <budidirajeshgoud231@gmail.com>
2024-07-13 19:03:05 +0530

    Update generateToken.js

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -1,20 +1,21 @@
-import jwt from 'jsonwebtoken'
+import jwt from 'jsonwebtoken';
 
+const generateToken = (res, userId) => {
+    // Generate JWT token
+    const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
+        expiresIn: '30d',
+    });
 
-const generateToken= (res, userId)=>{
-    const token = jwt.sign({userId}, process.env.JWT_SECRET, {
-        expiresIn: '30d'
-        })
+    // Set token as an HTTP-only cookie
+    res.cookie('jwt', token, {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === 'production', // true for production
+        sameSite: 'none', // Required for cross-origin requests
+        maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days
+    });
 
-    console.log(token);
-
-        //set token through server as http cookie, not directly to client local storgae
-        res.cookie('jwt', token, {
-    httpOnly: true,
-    secure: process.env.NODE_ENV === 'production', // true for production
-    sameSite: 'none', // Required for cross-origin requests
-    maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days
-});
-}
+    // Set status and response message
+    res.status(200).json({ message: 'Token generated and set in cookie' });
+};
 
 export default generateToken;
