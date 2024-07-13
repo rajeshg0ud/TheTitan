b7b9720627198f6878151b826fcebbed286cca0d
unknown <budidirajeshgoud231@gmail.com>
2024-07-13 18:26:13 +0530

    Updated constants folder

--- frontend/src/Constants.js
+++ frontend/src/Constants.js
@@ -1,2 +1,3 @@
-export const BASE_URL = process.env.NODE_ENV === 'development' ? 'http://localhost:5000' : 'https://thetitan-858v.onrender.com';
+export const BASE_URL = process.env.NODE_ENV === 'development' ? 'http://localhost:5000' : 'https://thetitan-production.up.railway.app/';
+//https://thetitan-858v.onrender.com';
 export const USER_URL = '/api/userRouter';
