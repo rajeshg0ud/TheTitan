2072f9ab0512e7199d0388979b125fc2a5afc9d4
Rajesh <budidirajeshgoud231@gmail.com>
2024-07-13 18:36:43 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,26 +1,34 @@
 import express from 'express';
-import productRouter from './routes/productRouter.js'
+import productRouter from './routes/productRouter.js';
 import userRouter from './routes/userRouter.js';
 import orderRouter from './routes/orderRouter.js';
 import cookieParser from 'cookie-parser';
 import dotenv from 'dotenv';
-dotenv.config();
+import cors from 'cors';
 import connectToDB from './db.js'; 
 import path from 'path';
-import cors from 'cors';
+
+dotenv.config();
 
 const app = express();
 const PORT = 5000;
+
 connectToDB();
 
 app.use(cors({
-    origin:   'https://the-titan.vercel.app' ,
+    origin: 'https://the-titan.vercel.app',
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With', 'Accept'],
-    credentials: true
+    credentials: true  
 }));
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
+app.use('/api/userRouter', userRouter);
+app.use('/api/orderRouter', orderRouter);
+app.use('/api/productRouter', productRouter);
 
 if (process.env.NODE_ENV === 'production') {
     const __dirname = path.resolve();
@@ -34,13 +42,11 @@ if (process.env.NODE_ENV === 'production') {
         res.send('API is running...');
     });
 }
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(cookieParser());
 
-app.use('/api/userRouter', userRouter);
-app.use('/api/orderRouter', orderRouter);
-app.use('/api/productRouter', productRouter);
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
+
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
