43e6d33bc7752935a6f2e8fa52a5de299297b968
Rajesh <budidirajeshgoud231@gmail.com>
2024-07-14 14:09:55 +0530

    Update generateToken.js

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -1,18 +1,17 @@
 import jwt from 'jsonwebtoken';
 
-
 const generateToken = (res, userId) => {
     // Generate JWT token
     const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
         expiresIn: '30d',
     });
-
-   console.log(token);
-
+console.log(token)
+    // Set token as an HTTP-only cookie
     res.cookie('jwt', token, {
         httpOnly: true,
-        secure: process.env.NODE_ENV === 'production', // Ensure secure cookies in production
-        sameSite: 'None', // For cross-origin requests
+        secure: process.env.NODE_ENV === 'production',  
+        sameSite: 'none',  
+        maxAge: 30 * 24 * 60 * 60 * 1000,  
     });
  
 };
