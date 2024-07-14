13735d8976574a78de04ccad675d5deda46b17d1
Rajesh <budidirajeshgoud231@gmail.com>
2024-07-14 15:05:57 +0530

    Update generateToken.js

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -10,7 +10,7 @@ console.log(token)
     res.cookie('jwt', token, {
         httpOnly: true,
         secure: process.env.NODE_ENV === 'production',  
-        sameSite: 'none',  
+        sameSite: 'None',  
         maxAge: 30 * 24 * 60 * 60 * 1000,  
     });
  
